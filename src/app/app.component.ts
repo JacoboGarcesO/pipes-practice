ef3ae9031aecd94bb49b3db87607683af1b98217
Jacobo Garcés <jacobo.garces@booktofly.com.co>
2021-06-08 09:16:21 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Capitán América';
+  nombre2 = 'cApItAn aMeRiCa';
+  personajes: string[] = ['Inronman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+  fecha: Date = new Date();
+  idioma = 'en';
+  promesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llegó');
+    }, 4500);
+  });
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
+
+  lenguajeEnglish(): void {
+    this.idioma = 'en';
+  }
+
+  lenguajeFrances(): void {
+    this.idioma = 'fr';
+  }
+
+  lenguajeSpanish(): void {
+    this.idioma = 'es';
+  }
 }
